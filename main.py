d4eef19d8f0ff27e8b32f4bea7ea4c189c5bde6c
Ethan Quisenberry <72354627+o-libSyntax-o@users.noreply.github.com>
2023-05-05 00:28:58 -0600

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,27 @@ password = os.environ["SSH_PASSWORD"]
 
 # Update the server and install basics, then run a setup script for docker and vpn
 setup_commands = [
-    "suto apt-get update -y",
+    "sudo apt-get update -y",
     "sudo apt-get upgrade -y",
     "sudo apt-get install curl -y",
+    "rm -r setup.sh",
     "curl -O https://raw.githubusercontent.com/o-libSyntax-o/server_control/main/setup.sh",
     "bash setup.sh",
 ]
 
+
+
 # Connect to the remote server over SSH
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-try:
-    ssh_client.connect(hostname=host, port=port, username=username, password=password)
 
+try:
+    ssh_client.connect(
+        hostname=host,
+        port=port,
+        username=username,
+        password=password,
+    )
     # Execute the setup commands on the remote server
     for command in setup_commands:
         stdin, stdout, stderr = ssh_client.exec_command(command)
